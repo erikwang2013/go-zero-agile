3c10b339a9a2a7a9587b9619f3aaac2c96c83570
erik <407978933@qq.com>
2022-10-06 14:43:09 +0800

    dev

--- system/admin/api/internal/middleware/adminmiddleware.go
+++ system/admin/api/internal/middleware/adminmiddleware.go
@@ -1,9 +1,13 @@
 package middleware
 
 import (
+	"context"
+	dataFormat "erik-agile/common/data-format"
+	"erik-agile/common/errorx"
+	"fmt"
 	"net/http"
 
-	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 type AdminMiddleware struct {
@@ -13,12 +17,31 @@ func NewAdminMiddleware() *AdminMiddleware {
     return &AdminMiddleware{}
 }
 
+//获取用户id
+func GetAdminId(ctx context.Context) int {
+    adminId := ctx.Value("admin_id")
+    getAdminId := fmt.Sprintf("%v\n", adminId)
+    return dataFormat.StringToInt(getAdminId)
+}
+
+//校验权限
+func CheckPermission(checkStr string) bool {
+    return true
+}
+
 func (m *AdminMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
-        logx.Error("打印中间件")
     return func(w http.ResponseWriter, r *http.Request) {
-    logx.Error("打印url")
-    //getUrl:=r.RequestURI
-    //getMethod:=r.Method
+        getToken := r.Header.Get("Authorization")
+        if len(getToken) <= 0 {
+            httpx.Error(w, errorx.NewCodeError(401000, "令牌认证失败"))
+            return
+        }
+        checkStr := dataFormat.GetMd5(r.RequestURI + r.Method)
+        result := CheckPermission(checkStr)
+        if false == result {
+            httpx.Error(w, errorx.NewCodeError(403000, "非法授权"))
+            return
+        }
         next(w, r)
     }
 }
